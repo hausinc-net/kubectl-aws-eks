ab56bd2ebf44b500cf6cb5b10157f4ff5a7fe140
acarey-haus <85202642+acarey-haus@users.noreply.github.com>
2021-07-02 14:36:53 -0700

    add status flag and results variable for output

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,13 @@ set -e
 echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
 
-sh -c "kubectl $*"
+#sh -c "kubectl $*"
+
+result="$(sh -c kubectl $1)"
+
+status=$?
+echo ::set-output name=result::$result
+echo "$result"
+if [[ $status -eq 0 ]]; then exit 0; else exit 1; fi
+
+
